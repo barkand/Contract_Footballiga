8c0e7bcfb1a8274bf72d3d4c91e2d4e00dd82c52
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2023-01-31 16:35:14 +0330

    Refactor Market

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -1,39 +1,33 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <8.13.0;
+pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Market is ERC721URIStorage, Ownable {
-    address public creator;
+    address private creator;
 
     constructor() ERC721("Footballiga", "FBGA") {
         creator = msg.sender;
     }
 
-    function buy(string memory _tokenURI, uint256 _itemId) external payable {
-        uint256 _tokenId = 14010000 + _itemId;
-        string memory newTokenURI = _tokenURI;
-
+    function buy(string memory _tokenURI, uint256 _tokenId) external payable {
         _safeMint(msg.sender, _tokenId);
-        _setTokenURI(_tokenId, newTokenURI);
+        _setTokenURI(_tokenId, _tokenURI);
 
         require(ownerOf(_tokenId) == msg.sender);
         payable(creator).transfer(msg.value);
     }
 
-    function getURI(uint256 _itemId) public view returns (string memory) {
-        uint256 _tokenId = 14010000 + _itemId;
+    function getURI(uint256 _tokenId) public view returns (string memory) {
         return tokenURI(_tokenId);
     }
 
-    function getOwnerToken(uint256 _itemId) public view returns (address) {
-        uint256 _tokenId = 14010000 + _itemId;
+    function getOwnerToken(uint256 _tokenId) public view returns (address) {
         return ownerOf(_tokenId);
     }
 
-    function transferNft(uint256 _itemId) external payable {
-        uint256 _tokenId = 14010000 + _itemId;
+    function transferNft(uint256 _tokenId) external payable {
         address owner = ownerOf(_tokenId);
         _transfer(owner, msg.sender, _tokenId);
 
